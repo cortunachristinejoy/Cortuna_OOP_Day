164a4f490788bb01b24573bd99e6e8bc4d852f12
cortunachristinejoy <159901419+cortunachristinejoy@users.noreply.github.com>
2024-03-12 15:38:29 +0800

    Add files via upload

---
+++ CORTUNA_RPG/src/cortuna_rpg/Paladin.java
@@ -0,0 +1,39 @@
+
+package cortuna_rpg;
+
+
+public class Paladin extends Swordsman {
+    
+    private boolean hasResurrected;
+    
+    public boolean isHasRessurected(){
+        return hasResurrected;
+    }
+    
+    public Paladin(){
+        super();
+        boolean hasResurrected = false;
+    }
+    
+    
+    
+    
+    public void receiveDamage (int damage){
+        if (damage % 2 == 0){
+            resurrect ();
+        }
+    }
+    
+    public void resurrect(){
+        if (hasResurrected){
+            hasResurrected = true;
+            setHealth(100);
+        } else {
+            System.out.println("Paladin has died");
+        }
+    }
+    
+}
+
+    
+
